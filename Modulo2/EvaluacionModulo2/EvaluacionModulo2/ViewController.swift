79b4208bc18dd8d4b1ebca183b8aa0302a73b952
jaimelombanag <alombana24@hotmail.com>
2018-05-21 20:25:19 -0500

    Se sube el programa con las imagenes en assent

---
+++ Modulo2/EvaluacionModulo2/EvaluacionModulo2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EvaluacionModulo2
+//
+//  Created by Jaime Lombana on 21/05/18.
+//  Copyright © 2018 Jaime Lombana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
